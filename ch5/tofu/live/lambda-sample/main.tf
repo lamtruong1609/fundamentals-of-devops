e93d2a9575798df3b50780bb6cbe4e83c3f43254
lamtruong1609 <lamtruong1609@users.noreply.github.com>
2024-10-01 11:53:50 +0000

    Automated Change - style: format files

--- ch5/tofu/live/lambda-sample/main.tf
+++ ch5/tofu/live/lambda-sample/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 terraform {
   backend "s3" {
     # TODO: fill in your own bucket name here!
-    bucket         = "fundamentals-of-devsecops-tofu-state"
-    key            = "ch5/tofu/live/lambda-sample"
-    region         = "us-west-2"
-    encrypt        = true
+    bucket  = "fundamentals-of-devsecops-tofu-state"
+    key     = "ch5/tofu/live/lambda-sample"
+    region  = "us-west-2"
+    encrypt = true
     # TODO: fill in your own DynamoDB table name here!
     dynamodb_table = "fundamentals-of-devsecops-tofu-state"
   }
