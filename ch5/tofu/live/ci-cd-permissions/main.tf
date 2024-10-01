ca6e833f3b0f65113e4f98f1337fbae2966fc7a2
Lam Truong <lamtruong2354@gmail.com>
2024-10-01 19:58:22 +0700

    Add deployment workflow

--- ch5/tofu/live/ci-cd-permissions/main.tf
+++ ch5/tofu/live/ci-cd-permissions/main.tf
@@ -13,8 +13,14 @@ module "iam_roles" {
 
   name = "lambda-sample"
   oidc_provider_arn = module.oidc_provider.oidc_provider_arn
+
   enable_iam_role_for_testing = true
+  enable_iam_role_for_plan  = true
+  enable_iam_role_for_apply = true
 
   github_repo = "lamtruong1609/fundamentals-of-devops"
   lambda_base_name = "lambda-sample"
+
+  tofu_state_bucket         = "fundamentals-of-devsecops-tofu-state"
+  tofu_state_dynamodb_table = "fundamentals-of-devsecops-tofu-state"
 }
