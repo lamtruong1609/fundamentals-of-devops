2db117e6309eaabb5edf4e9786a71ee0a5acd021
lamtruong1609 <lamtruong1609@users.noreply.github.com>
2024-10-01 08:58:40 +0000

    Automated Change - style: format files

--- ch5/tofu/live/lambda-sample/main.tf
+++ ch5/tofu/live/lambda-sample/main.tf
@@ -7,14 +7,14 @@ module "function" {
 
   name = var.name
 
-  src_dir = "${path.module}/src" 
-  runtime = "nodejs20.x"         
-  handler = "index.handler"      
+  src_dir = "${path.module}/src"
+  runtime = "nodejs20.x"
+  handler = "index.handler"
 
-  memory_size = 128              
-  timeout     = 5                
+  memory_size = 128
+  timeout     = 5
 
-  environment_variables = {      
+  environment_variables = {
     NODE_ENV = "production"
   }
 
@@ -24,6 +24,6 @@ module "gateway" {
   source = "github.com/brikis98/devops-book//ch3/tofu/modules/api-gateway"
 
   name               = var.name
-  function_arn       = module.function.function_arn 
-  api_gateway_routes = ["GET /"]                    
+  function_arn       = module.function.function_arn
+  api_gateway_routes = ["GET /"]
 }
